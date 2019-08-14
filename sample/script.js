4eab18dc9309e00f8c487ee11b4785f2bbf7ae35
rafal95 <47984361+rafal95@users.noreply.github.com>
2019-08-14 16:13:15 +0200

    Add files via upload

---
+++ sample/script.js
@@ -0,0 +1,17 @@
+var arrow = document.querySelector('.arrow');
+var search = document.querySelector('.search-icon');
+
+function show(){
+    var menu_left = document.querySelector('.menu_left_show');
+    var nav_left = document.querySelector('.nav_left');
+    menu_left.classList.replace('menu_left_show', 'menu_left_hidden');
+    nav_left.classList.toggle('hidden');
+}
+
+function toggleShow(){
+    var input = document.querySelector('.search input');
+    input.classList.toggle('hidden');
+}
+
+search.onclick = toggleShow;
+arrow.onclick = show;
